8181fc8833c3fb170682ea0db578d8d079d6fb4e
androidkof <androiddev@kicksonfire.com>
2016-03-11 17:44:33 +0530

    Custom Ratebar

--- CustomeRatebar/app/src/main/java/testfacebookshare/customeratebar/MainActivity.java
+++ CustomeRatebar/app/src/main/java/testfacebookshare/customeratebar/MainActivity.java
@@ -1,12 +1,11 @@
 package testfacebookshare.customeratebar;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RatingBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
                 ratingBar_default1.setRating(5);
 
 
-                Toast.makeText(MainActivity.this,"This is testing Toast message", Toast.LENGTH_LONG).show();
+
 
                 text.setText("Rating Large:" + String.valueOf(ratingBar_default.getRating())+"\nRating Small:"+String.valueOf(ratingBar_default1.getRating()));
             }
